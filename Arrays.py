2791136a6d6dd83a31329a7578b8b5103335f904
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-05-05 22:01:33 -0400

    First to main

---
+++ Arrays.py
@@ -0,0 +1,27 @@
+# Arrays Strings
+names = ["Abraham", "Ashley", "Aaliyah"]
+
+print(names)
+
+print()
+print(names[0])
+print(names[1])
+print(names[2])
+print()
+print(names[0:2])
+print()
+
+# Arrays Integer
+numbers = [1,2,3,4,5]
+
+print("Original is: ")
+print(numbers)
+print()
+
+print("Using append(): ")
+numbers.append(6)
+print(numbers)
+print()
+
+print("using 'len' which is the total count of each array: ")
+print(len(numbers))
